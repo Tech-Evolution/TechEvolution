b1c6dbe4d44fd84ec3b6d43622e2650cfac8203b
MaoBackDev <mauricio.desarrollador@gmail.com>
2022-10-27 19:14:36 -0500

    carrito

--- frontend/src/views/Cart.jsx
+++ frontend/src/views/Cart.jsx
@@ -2,50 +2,51 @@ const Cart = () => {
   return (
     <>
     <div className="container py-5">
+        <div className="py-2"></div>
         <main classNameName="py-5">
             <div className="py-5 text-center">
-                <img className="d-block mx-auto mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
-                <h2>Checkout form</h2>
-                <p className="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
+                <img className="d-block mx-auto mb-4" src="/assets/cart.jpg" alt="" width="200" height="150" />
+                <h2>Formulario de Pago</h2>
+                <p className="lead">A continuación se muestra un formulario de ejemplo construido completamente con los controles de formulario de Bootstrap. Cada grupo de formulario requerido tiene un estado de validación que se puede activar al intentar enviar el formulario sin completarlo.</p>
             </div>
 
             <div className="row g-5">
                 <div className="col-md-5 col-lg-4 order-md-last">
                     <h4 className="d-flex justify-content-between align-items-center mb-3">
-                    <span className="text-primary">Your cart</span>
+                    <span className="text-primary">Tu Carrito</span>
                     <span className="badge bg-primary rounded-pill">3</span>
                     </h4>
                     <ul className="list-group mb-3">
                         <li className="list-group-item d-flex justify-content-between lh-sm">
                             <div>
-                            <h6 className="my-0">Product name</h6>
-                            <small className="text-muted">Brief description</small>
+                            <h6 className="my-0">Primer Prducto</h6>
+                            <small className="text-muted">Descripción</small>
                             </div>
                             <span className="text-muted">$12</span>
                         </li>
                         <li className="list-group-item d-flex justify-content-between lh-sm">
                             <div>
-                            <h6 className="my-0">Second product</h6>
-                            <small className="text-muted">Brief description</small>
+                            <h6 className="my-0">Segundo producto</h6>
+                            <small className="text-muted">Descripción</small>
                             </div>
                             <span className="text-muted">$8</span>
                         </li>
                         <li className="list-group-item d-flex justify-content-between lh-sm">
                             <div>
-                            <h6 className="my-0">Third item</h6>
-                            <small className="text-muted">Brief description</small>
+                            <h6 className="my-0">Tercer Producto</h6>
+                            <small className="text-muted">Descripción</small>
                             </div>
                             <span className="text-muted">$5</span>
                         </li>
                         <li className="list-group-item d-flex justify-content-between bg-light">
                             <div className="text-success">
-                            <h6 className="my-0">Promo code</h6>
-                            <small>EXAMPLECODE</small>
+                            <h6 className="my-0">Código Promocional</h6>
+                            <small>102383</small>
                             </div>
                             <span className="text-success">−$5</span>
                         </li>
                         <li className="list-group-item d-flex justify-content-between">
-                            <span>Total (USD)</span>
+                            <span>Total:</span>
                             <strong>$20</strong>
                         </li>
                     </ul>
@@ -53,17 +54,17 @@ const Cart = () => {
                     <form className="card p-2">
                         <div className="input-group">
                             <input type="text" className="form-control" placeholder="Promo code"/>
-                            <button type="submit" className="btn btn-secondary">Redeem</button>
+                            <button type="submit" className="btn btn-secondary">Redimir</button>
                         </div>
                     </form>
                 </div>
 
                 <div className="col-md-7 col-lg-8">
-                    <h4 className="mb-3">Billing address</h4>
+                    <h4 className="mb-3">Dirección de Envío</h4>
                      <form className="needs-validation" novalidate>
                         <div className="row g-3">
                             <div className="col-sm-6">
-                                <label for="firstName" className="form-label">First name</label>
+                                <label for="firstName" className="form-label">Nombres</label>
                                 <input type="text" className="form-control" id="firstName" placeholder="" value="" required/>
                                 <div className="invalid-feedback">
                                     Valid first name is required.
@@ -71,7 +72,7 @@ const Cart = () => {
                             </div>
 
                             <div className="col-sm-6">
-                                <label for="lastName" className="form-label">Last name</label>
+                                <label for="lastName" className="form-label">Apellidos</label>
                                 <input type="text" className="form-control" id="lastName" placeholder="" value="" required/>
                                 <div className="invalid-feedback">
                                     Valid last name is required.
@@ -79,7 +80,7 @@ const Cart = () => {
                             </div>
 
                             <div className="col-12">
-                                <label for="username" className="form-label">Username</label>
+                                <label for="username" className="form-label">Nombre de Usuario</label>
                                 <div className="input-group has-validation">
                                     <span className="input-group-text">@</span>
                                     <input type="text" className="form-control" id="username" placeholder="Username" required/>
@@ -90,7 +91,7 @@ const Cart = () => {
                             </div>
 
                             <div className="col-12">
-                                <label for="email" className="form-label">Email <span className="text-muted">(Optional)</span></label>
+                                <label for="email" className="form-label">Correo Electrónico <span className="text-muted">(Opcional)</span></label>
                                 <input type="email" className="form-control" id="email" placeholder="you@example.com"/>
                                 <div className="invalid-feedback">
                                     Please enter a valid email address for shipping updates.
@@ -98,7 +99,7 @@ const Cart = () => {
                             </div>
 
                             <div className="col-12">
-                                <label for="address" className="form-label">Address</label>
+                                <label for="address" className="form-label">Dirección</label>
                                 <input type="text" className="form-control" id="address" placeholder="1234 Main St" required/>
                                 <div className="invalid-feedback">
                                     Please enter your shipping address.
@@ -106,37 +107,20 @@ const Cart = () => {
                             </div>
 
                             <div className="col-12">
-                                <label for="address2" className="form-label">Address 2 <span className="text-muted">(Optional)</span></label>
+                                <label for="address2" className="form-label">Dirección 2 <span className="text-muted">(Opcional)</span></label>
                                 <input type="text" className="form-control" id="address2" placeholder="Apartment or suite"/>
                             </div>
 
                             <div className="col-md-5">
-                                <label for="country" className="form-label">Country</label>
+                                <label for="country" className="form-label">Cíudad</label>
                                 <select className="form-select" id="country" required>
                                     <option value="">Choose...</option>
-                                    <option>United States</option>
+                                    <option>Medellín</option>
+                                    <option>Cali</option>
+                                    <option>Bogotá</option>
                                 </select>
                                 <div className="invalid-feedback">
                                     Please select a valid country.
-                                </div>
-                            </div>
-
-                            <div className="col-md-4">
-                                <label for="state" className="form-label">State</label>
-                                <select className="form-select" id="state" required>
-                                    <option value="">Choose...</option>
-                                    <option>California</option>
-                                </select>
-                                <div className="invalid-feedback">
-                                    Please provide a valid state.
-                                </div>
-                            </div>
-
-                            <div className="col-md-3">
-                                <label for="zip" className="form-label">Zip</label>
-                                <input type="text" className="form-control" id="zip" placeholder="" required/>
-                                <div className="invalid-feedback">
-                                    Zip code required.
                                 </div>
                             </div>
                         </div>
@@ -145,12 +129,12 @@ const Cart = () => {
 
                         <div className="form-check">
                             <input type="checkbox" className="form-check-input" id="same-address"/>
-                            <label className="form-check-label" for="same-address">Shipping address is the same as my billing address</label>
+                            <label className="form-check-label" for="same-address">La dirección de envío es la misma que mi dirección de facturación address</label>
                         </div>
 
                         <div className="form-check">
                             <input type="checkbox" className="form-check-input" id="save-info"/>
-                            <label className="form-check-label" for="save-info">Save this information for next time</label>
+                            <label className="form-check-label" for="save-info">Guarda esta información para la próxima vez</label>
                         </div>
 
                         <hr className="my-4"/>
@@ -160,11 +144,11 @@ const Cart = () => {
                         <div className="my-3">
                             <div className="form-check">
                                 <input id="credit" name="paymentMethod" type="radio" className="form-check-input" checked required/>
-                                <label className="form-check-label" for="credit">Credit card</label>
+                                <label className="form-check-label" for="credit">Tarjeta de crédito</label>
                             </div>
                             <div className="form-check">
                                 <input id="debit" name="paymentMethod" type="radio" className="form-check-input" required/>
-                                <label className="form-check-label" for="debit">Debit card</label>
+                                <label className="form-check-label" for="debit">Tarjeta de débito</label>
                             </div>
                             <div className="form-check">
                                 <input id="paypal" name="paymentMethod" type="radio" className="form-check-input" required/>
@@ -174,16 +158,16 @@ const Cart = () => {
 
                         <div className="row gy-3">
                             <div className="col-md-6">
-                                <label for="cc-name" className="form-label">Name on card</label>
+                                <label for="cc-name" className="form-label">Nombre en la tarjeta</label>
                                 <input type="text" className="form-control" id="cc-name" placeholder="" required/>
-                                <small className="text-muted">Full name as displayed on card</small>
+                                <small className="text-muted">Nombre completo como se muestra en la tarjeta</small>
                                 <div className="invalid-feedback">
                                     Name on card is required
                                 </div>
                             </div>
 
                             <div className="col-md-6">
-                                <label for="cc-number" className="form-label">Credit card number</label>
+                                <label for="cc-number" className="form-label">Número de Tarjeta de Crédito</label>
                                 <input type="text" className="form-control" id="cc-number" placeholder="" required/>
                                 <div className="invalid-feedback">
                                     Credit card number is required
@@ -191,7 +175,7 @@ const Cart = () => {
                             </div>
 
                             <div className="col-md-3">
-                                <label for="cc-expiration" className="form-label">Expiration</label>
+                                <label for="cc-expiration" className="form-label">Vencimiento</label>
                                 <input type="text" className="form-control" id="cc-expiration" placeholder="" required/>
                                 <div className="invalid-feedback">
                                     Expiration date required
@@ -209,7 +193,7 @@ const Cart = () => {
 
                         <hr className="my-4"/>
 
-                        <button className="w-100 btn btn-primary btn-lg" type="submit">Continue to checkout</button>
+                        <button className="w-100 btn btn-primary btn-lg" type="submit">Continuar con la aprobación</button>
                     </form>
                 </div>
             </div>
