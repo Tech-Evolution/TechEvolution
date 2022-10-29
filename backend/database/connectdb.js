cbf23e62b3de22ece89725287a137242d093f33c
MaoBackDev <mauricio.desarrollador@gmail.com>
2022-10-29 15:20:26 -0500

    Inicio Api

--- backend/database/connectdb.js
+++ backend/database/connectdb.js
@@ -7,3 +7,9 @@ try {
 } catch (error){
     console.log("Error de conexión -> " + error)
 }
+
+// Other option to connect database
+// mongoose
+//     .connect(process.env.URI_MONGO)
+//     .then(() => console.log("Connection Db ok ✌"))
+//     .catch((error) => console.log("Error de conexión -> " + error))
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 import 'dotenv/config' // enviroment variables
-import './backend/database/connectdb.js' // Database conectction module
+import './backend/database/connectdb.js' // Database conecction module
 import express from 'express'  // Import express
 
 import productRouter from './backend/routes/product.route.js'
+import userRouter from './backend/routes/users.route.js' 
+import authRouter from './backend/routes/auth.route.js'
 
 const PORT = process.env.PORT 
 const app = express()           // object express
 
-app.use(express.json())  // Permite leer las solicitidues en json
+app.use(express.json())  // read request in json format
 
 app.use('/techevolution/products', productRouter)
-
-// Archivos estaticos
-// app.use(express.static('public'))
+app.use('/techevolution/users', userRouter)
+app.use('/techevolution/auth', authRouter)
 
 
 // Listen method to server connection
-app.listen(PORT, () => {
+app.listen(PORT || 5000, ()  => {
     console.log("✔✔✔ https://localhost: " + PORT)
 })
